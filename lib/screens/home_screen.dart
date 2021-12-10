bb621905864a0d10803f6ea986b07df564f6aca9
UrielNunez <Devuriel1@gmail.com>
2021-12-10 17:47:33 -0600

    primer commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
+import 'package:peliculas/search/search_delegate.dart';
+import 'package:peliculas/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+
+
+class HomeScreen extends StatelessWidget {
+
+  
+  @override
+  Widget build(BuildContext context) {
+
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+    
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text('Películas en Cines'),
+        elevation: 0,
+        actions: [
+          IconButton(
+             
+            icon: Icon(Icons.search_outlined),
+            onPressed: () => showSearch(context: context, delegate: MovieSearchDelegate () ),
+          )
+          
+        ],
+
+      ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+          
+            //Tarjetas Pricipales
+            CardSwiper( movies: moviesProvider.onDisplayMovies ),
+
+            //Slider de Pelculas
+            MovieSlider( 
+              movies: moviesProvider.popularMovies,
+              title: 'Populares' ,
+              onNextPage: () => moviesProvider.getPopularMovies(),
+            ),
+
+
+          ],
+        )
+      )
+    );
+  }
+}
